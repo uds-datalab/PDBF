f782ca0dc3aa97752c2137d730ff136fc8d9d03e
Patrick Bender <ichbinkeinreh@t-online.de>
2015-02-20 15:58:44 +0100

    !

--- src/pdbf/latex/LaTeX_Compiler.java
+++ src/pdbf/latex/LaTeX_Compiler.java
@@ -1,6 +1,33 @@
 package pdbf.latex;
 
+import java.io.File;
+
 public class LaTeX_Compiler {
-    
-  //sp to mm -> 5,36346435546875e-6
+
+    private static final String pathToTexWorks = "C:\\Program Files (x86)\\MiKTeX 2.9\\miktex\\bin\\texworks.exe";
+
+    public static void main(String[] args) {
+	if (args.length != 1) {
+	    System.out
+		    .println("Usage: Java_Compiler.jar pathToPDF "
+			    + System.lineSeparator()
+			    + "Assuming that the config.json file is in the same folder");
+	    System.exit(-1);
+	}
+	String latexPath = args[0];
+	File latex = new File(latexPath);
+	if (!latex.exists()) {
+	    System.err.println("Error: Source PDF file does not exist!");
+	    System.exit(-1);
+	}
+	
+	try {
+	    Process p = Runtime.getRuntime().exec(pathToTexWorks + " " + latex);
+	    p.waitFor();
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+	// sp to mm -> 5,36346435546875e-6
+    }
+
 }
