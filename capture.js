1a3f8fb5b9851893b5f0988cb0f02e09ca957875
Patrick Bender <ichbinkeinreh@t-online.de>
2015-02-26 00:37:39 +0100

    - removed some unused stuff from test.tex - adjusted scale of chart images to html size - chart legend position fixed

--- capture.js
+++ capture.js
@@ -12,7 +12,7 @@ page.open(system.args[1], function() {
 	var h = page.evaluate(function() {
 		return outh;
 	});
-	page.zoomFactor = 1.75;
+	page.zoomFactor = 2.5;
 	page.viewportSize = {
 		width: w,
 		height: h
