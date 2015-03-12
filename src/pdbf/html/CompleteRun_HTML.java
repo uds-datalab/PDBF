1074a177e2c99bb3d58361ab8c8ad8107c116723
Patrick Bender <ichbinkeinreh@t-online.de>
2015-03-12 09:31:33 +0100

    added beamer testfile

--- src/pdbf/html/CompleteRun_HTML.java
+++ src/pdbf/html/CompleteRun_HTML.java
@@ -9,11 +9,25 @@ public class CompleteRun_HTML {
     public static void main(String[] args) {
 	LaTeX_Compiler.main(args);
 	HTML_Compiler.main(args);
+	
 	new File("dim.json").delete();
 	new File("config.json").delete();
 	new File("db.sql").delete();
 	new File("db.json").delete();
 	new File("null.png").delete();
+	
+	new File("test.aux").delete();
+	new File("test.log").delete();
+	new File("test.synctex.gz").delete();
+	
+	new File("beamer_test.aux").delete();
+	new File("beamer_test.log").delete();
+	new File("beamer_test.nav").delete();
+	new File("beamer_test.out").delete();
+	new File("beamer_test.snm").delete();
+	new File("beamer_test.toc").delete();
+	new File("beamer_test.synctex.gz").delete();
+	
 	System.out.println("Finished!");
     }
     
