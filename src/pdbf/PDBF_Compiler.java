900e1d5dd5ca0a0d4a6915a2a7d51e9c05d2959c
Patrick Bender <ichbinkeinreh@t-online.de>
2016-02-24 18:56:07 +0100

    Improved commandline parsing

--- src/pdbf/PDBF_Compiler.java
+++ src/pdbf/PDBF_Compiler.java
@@ -4,7 +4,15 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-
+import org.apache.commons.cli.AlreadySelectedException;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionGroup;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.BasicConfigurator;
@@ -15,45 +23,25 @@ import pdbf.compilers.HTML_PDF_Compiler;
 import pdbf.compilers.Pre_Compiler;
 import pdbf.compilers.TAR_Compiler;
 import pdbf.compilers.VM_Compiler;
-import pdbf.misc.MinifyResources;
 import pdbf.misc.Tools;
 
 public class PDBF_Compiler {
 
-    // Debug Flag. Cannot be changed by User
-    public static boolean includeRes = false;
+    // This flag indicates if all js and css files from data folder are included
+    // in the compiled PDBF document
+    public static boolean includeRes = true;
 
-    private static void showHelp() {
-	System.out
-		.println("Usage:\n"
-			+ "java -jar pdbf.jar LaTeX_file [options]\n"
-			+ "Possible options:\n"
-			+ "--no-pdf-protection : Disables the protection of the pdf part of the PDBF file\n"
-			+ "OR\n"
-			+ "java -jar pdbf.jar --vm  PDBF_File.html VM_File.ova\n"
-			+ "OR\n"
-			+ "java -jar pdbf.jar --tar  PDBF_File.html TAR_File.tar\n"
-			+ "For further help visit: https://github.com/uds-datalab/PDBF");
-	System.exit(0);
-    }
-
-    public static void compile(String[] args) {
-	Pre_Compiler.main(args);
-	HTML_PDF_Compiler.main(args);
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AlreadySelectedException {
 	// Apache PDFBox uses the Logger class and we need to configure it
 	BasicConfigurator.configure();
 	Logger.getRootLogger().setLevel(Level.ERROR);
 
-	//
+	// Get name of the operating system
 	Pre_Compiler.suffix = Tools.getOS();
 
 	String version = null;
 	try {
-	    version = FileUtils.readFileToString(new File(Tools.getBaseDir()
-		    + "VERSION.md"));
+	    version = FileUtils.readFileToString(new File(Tools.getBaseDir() + "VERSION.md"));
 	} catch (IOException e1) {
 	    e1.printStackTrace();
 	    System.exit(1);
@@ -61,53 +49,78 @@ public class PDBF_Compiler {
 
 	// Check for updates
 	try {
-	    URL url = new URL(
-		    "https://raw.githubusercontent.com/uds-datalab/PDBF/gh-pages/VERSION.md");
-	    String currentVersion = IOUtils.toString(url.openStream(),
-		    StandardCharsets.UTF_8);
+	    URL url = new URL("https://raw.githubusercontent.com/uds-datalab/PDBF/gh-pages/VERSION.md");
+	    String currentVersion = IOUtils.toString(url.openStream(), StandardCharsets.UTF_8);
 	    if (!version.equals(currentVersion)) {
-		System.err
-			.println(currentVersion
-				+ " of PDBF compiler available.\n"
-				+ "Please visit https://github.com/uds-datalab/PDBF and download the latest version\n");
+		System.err.println(currentVersion + " of PDBF compiler available.\n"
+			+ "Please visit https://github.com/uds-datalab/PDBF and download the latest version\n");
 	    }
 	} catch (Throwable t) {
 	    // Do nothing
 	}
 
-	// To minimize the possibility that i forget to turn off includeRes
-	// before building a release ;)
-	if (!includeRes) {
-	    System.out.println("!!!!Warning!!!! includeRes is off");
-	}
 	// Get current time to measure the time it takes to complete the task
 	long begin = System.currentTimeMillis();
 
-	if (args[0].equalsIgnoreCase("--version")) {
-	    System.out
-		    .println("PDBF "
-			    + version
-			    + "\nVisit https://github.com/uds-datalab/PDBF for more information");
-	    System.exit(0);
-	} else if (args[0].equalsIgnoreCase("--help")) {
-	    showHelp();
-	} else if (args[0].equalsIgnoreCase("--tar") && args.length == 3) {
-	    TAR_Compiler.main(args);
-	    System.exit(0);
-	} else if (args[0].equalsIgnoreCase("--vm") && args.length == 3) {
-	    VM_Compiler.main(args);
-	    System.exit(0);
-	} else if (args.length == 1) {
-	    Pre_Compiler.pdfProtect = true;
-	    compile(args);
-	} else if (args.length == 2
-		&& args[1].equalsIgnoreCase("--no-pdf-protection")) {
-	    Pre_Compiler.pdfProtect = false;
-	    compile(args);
-	}
+	// Parse command line
+	CommandLineParser parser = new DefaultParser();
+	HelpFormatter formatter = new HelpFormatter();
+	Options options = new Options();
+	OptionGroup optionsGroup = new OptionGroup();
+	options.addOptionGroup(optionsGroup);
+	optionsGroup.addOption(Option.builder("c").longOpt("compile").numberOfArgs(1).argName("LaTeX_File.tex")
+		.desc("Compile a tex file to a PDBF document. This is the default option.").build());
+	options.addOption("npp", "no-pdf-protection", false,
+		"Disables the write protection of the pdf part of the PDBF document. Can only be used with the compile option.");
+	options.addOption("nir", "no-include-res", false, "Disables the inclusion of all js and css files."
+		+ " Produces a PDBF document that can only be opened when placed in the data folder of the PDBF framework."
+		+ " Can only be used with the compile option.");
+	optionsGroup.addOption(Option.builder("t").longOpt("tar").numberOfArgs(2).argName("PDBF_File.html> <TAR_File.tar")
+		.desc("Appends a tar archive to an existing PDBF document.").build());
+	options.addOption("v", "version", false, "Display version number");
+	options.addOption("h", "help", false, "Display this help message");
 
-	System.out.println("Finished!");
-	System.out.println("Took " + (System.currentTimeMillis() - begin)
-		+ "ms");
+	try {
+	    // Parse the command line arguments
+	    CommandLine line = parser.parse(options, args, false);
+	    if (line.hasOption("no-pdf-protection")) {
+		System.out.println("no-pdf-protection set");
+		Pre_Compiler.pdfProtect = false;
+	    } else {
+		Pre_Compiler.pdfProtect = true;
+	    }
+
+	    if (line.hasOption("no-include-res")) {
+		includeRes = false;
+	    } else {
+		includeRes = true;
+	    }
+
+	    if (line.hasOption("version")) {
+		System.out.println("PDBF " + version + "\nVisit https://github.com/uds-datalab/PDBF for more information");
+		System.exit(0);
+	    } else if (line.hasOption("help")) {
+		formatter.printHelp("java -jar pdbf.jar [options]", options);
+	    } else if (line.hasOption("tar")) {
+		TAR_Compiler.main(line.getOptionValues("tar"));
+	    } else if (line.hasOption("vm")) {
+		VM_Compiler.main(line.getOptionValues("vm"));
+	    } else if (line.hasOption("compile")) {
+		Pre_Compiler.main(line.getOptionValues("compile"));
+		HTML_PDF_Compiler.main(line.getOptionValues("compile"));
+	    } else if (args.length == 1) {
+		Pre_Compiler.main(args);
+		HTML_PDF_Compiler.main(args);
+	    } else {
+		formatter.printHelp("java -jar pdbf.jar [options]", options);
+		System.exit(0);
+	    }
+	    System.out.println("Finished!");
+	    System.out.println("Took " + (System.currentTimeMillis() - begin) + "ms");
+	    System.exit(0);
+	} catch (ParseException exp) {
+	    System.out.println(exp.getMessage());
+	    formatter.printHelp("java -jar pdbf.jar [options]", options);
+	}
     }
 }
